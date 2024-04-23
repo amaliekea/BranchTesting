a96b69c50721ae2d695ce55f62b7b2ae8a41e513
amaliekea <amje0001@stud.kea.dk>
2024-04-23 15:10:19 +0200

    implement mood

--- src/Cat.java
+++ src/Cat.java
@@ -8,4 +8,13 @@ public class Cat extends Animal {
     public boolean eat(String foodType) {
         return true;
     }
+
+    @Override
+    public void showMood(boolean mood) {
+        if (mood) {
+            System.out.println("spinning");
+        } else {
+            System.out.println("hvæser");
+        }
+    }
 }
